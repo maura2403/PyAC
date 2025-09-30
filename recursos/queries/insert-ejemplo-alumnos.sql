fb38dc1044c5e6df013956fcf82e2489c0c7dae7
maura2403 <maura2403@gmail.com>
2025-09-29 21:24:30 -0300

    Insertar un registro fijo con query desde el codigo para probar conexion a DB

---
+++ recursos/queries/insert-ejemplo-alumnos.sql
@@ -0,0 +1,4 @@
+DELETE FROM pyac.alumnos;
+
+INSERT INTO pyac.alumnos(id_alumno, apellido,nombre,curso,modalidad,responsable1,responsable2,responsable_de_pagos)
+VALUES (1,'González','Alicia','6to B','Eventual','Guido','Alejandra','María');
--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,15 @@
 import { Client } from 'pg'
+import { readFile } from 'node:fs/promises';
+import { resolve } from 'node:path';
+
 const client = new Client()
 await client.connect()
  
-const res = await client.query('SELECT $1::text as message', ['Hello world!'])
-console.log(res.rows[0].message) // Hello world!
+const filePath = resolve('./recursos/queries/insert-ejemplo-alumnos.sql');
+const contents = await readFile(filePath, { encoding: 'utf8' });
+console.log(contents);
+await client.query(contents);
+const res = await client.query('SELECT * FROM pyac.alumnos');
+console.log(res.rows[0]);
+
 await client.end()
