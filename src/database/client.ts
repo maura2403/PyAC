3aa87d1641bfb76928407077ab5a15ab1040297e
Romelai28 <Romelai28@gmail.com>
2025-11-03 00:55:24 -0300

    Integra uso de dotenv para el manejo de variables de entorno y actualiza la configuración de las conexiones a la base de datos

--- src/database/client.ts
+++ src/database/client.ts
@@ -1,7 +1,24 @@
-import { Client } from "pg";
+import 'dotenv/config';
+import { Client, Pool } from "pg";
+import type { ClientConfig } from "pg";
+
+
+const dbConfig: ClientConfig =
+    process.env.DATABASE_URL
+        ? { connectionString : process.env.DATABASE_URL }
+        : {
+            host: process.env.PGHOST,
+            port: parseInt(process.env.PGPORT || '5432'),
+            database: process.env.PGDATABASE,
+            user: process.env.PGUSER,
+            password: process.env.PGPASSWORD,
+        };
+
 
 export async function getDbClient(){
-    const clientDb = new Client();
+    const clientDb = new Client (dbConfig);
     await clientDb.connect();
     return clientDb;
 }
+
+export const poolDb = new Pool (dbConfig);
