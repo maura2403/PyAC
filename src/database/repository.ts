0a41d8a17c4fb838835f5a4c13a70d1cf78530e6
Emi <emiliano.testa.e@gmail.com>
2025-11-15 21:09:42 -0300

    Primera implementacion con repository y modelos

--- src/database/repository.ts
+++ src/database/repository.ts
@@ -1,6 +1,182 @@
 import { Pool } from "pg";
-import { accessAt, arraySameElements } from "../extra/utils.js";
 
+export abstract class Repository {
+    public get tableName(): string {
+        return this.model.tableName;
+    }
+    protected abstract readonly model: Model;
+    protected readonly pool: Pool;
+
+    constructor(pool: Pool) {
+        this.pool = pool;
+    }
+
+    public async create(row: Record<string, any>): Promise<void> {
+        const placeholders = this.model.allColumns.map((_, i) => `$${i + 1}`).join(', ');
+        const query = `
+            INSERT INTO ${this.model.schema}.${this.model.tableName} (${this.model.allColumns.join(', ')})
+            VALUES (${placeholders})
+        `;
+        const values = this.model.allColumns.map(key => row[key]);
+        await this.pool.query(query, values);
+    }
+
+    public async read(filters: Record<string, any>): Promise<Record<string, any>[]> {
+        let query = `SELECT * FROM ${this.model.schema}.${this.model.tableName}`;
+        const filterKeys = Object.keys(filters);
+
+        if (filterKeys.length > 0) {
+            const placeholders = filterKeys.map((key, i) => `${key} = $${i + 1}`).join(' AND ');
+            query += ` WHERE ${placeholders}`;
+        }
+
+        const values = filterKeys.map(key => filters[key]);
+        const items = await this.pool.query(query, values);
+        return items.rows;
+    }
+
+    public async update(originalPKs: Record<string, any>, row: Record<string, any>): Promise<void> {
+        const query = `
+            UPDATE ${this.model.schema}.${this.model.tableName}
+            SET ${this.model.allColumns.map((col, i) => `${String(col)} = $${i + this.model.primaryKeys.length + 1}`).join(', ')}
+            WHERE ${this.model.primaryKeys.map((key, i) => `${String(key)} = $${i + 1}`).join(' AND ')}
+        `;
+        const keyValues = this.model.primaryKeys.map(key => originalPKs[key]);
+        const allValues = this.model.allColumns.map(col => row[col]);
+        await this.pool.query(query, [...keyValues, ...allValues]);
+    }
+
+    public async delete(originalPKs: Record<string, any>): Promise<void> {
+        const primaryKeys = Object.keys(originalPKs);
+        const query = `
+            DELETE FROM ${this.model.schema}.${this.model.tableName}
+            WHERE ${primaryKeys.map((key, i) => `${key} = $${i + 1}`).join(' AND ')}
+        `
+        const values = primaryKeys.map(key => originalPKs[key]);
+        await this.pool.query(query, values);
+    }
+}
+
+export class StudentRepository extends Repository {
+    protected model: Model;
+    constructor(pool: Pool) {
+        super(pool);
+        this.model = new Model(
+            {
+                dni: { type: DatabaseType.INTEGER, options: { primaryKey: true } },
+                nombre: { type: DatabaseType.STRING, options: { } },
+                apellido: { type: DatabaseType.STRING, options: { } },
+                curso: { type: DatabaseType.STRING, options: { } },
+                modalidad: { type: DatabaseType.MODALIDADES, options: { } },
+                nivel: { type: DatabaseType.NIVELES, options: { } },
+                responsable_de_pagos: { type: DatabaseType.STRING, options: { allowNull: true } },
+                responsable1: { type: DatabaseType.STRING, options: { allowNull: true } }
+            },
+            "Pyac",
+            "Alumnos"
+        );
+    }
+}
+
+export class AttendanceRepository extends Repository {
+    protected readonly model = new Model(
+        {
+            dni: { type: DatabaseType.INTEGER, options: { primaryKey: true }},
+            fecha: { type: DatabaseType.DATE, options: { primaryKey: true }}
+        },
+        "Pyac",
+        "Presentes"
+    );
+}
+
+export class LevelRepository extends Repository {
+    protected readonly model = new Model(
+        {
+            nivel: { type: DatabaseType.NIVELES, options: { primaryKey: true }},
+            precio: { type: DatabaseType.FLOAT, options: { } }
+        },
+        "Pyac",
+        "Niveles"
+    );
+
+    public async getPrice(nivel: "Jardin" | "Primaria"): Promise<number> {
+        const rows = await this.read({ nivel : nivel });
+        return rows[0]!.precio;
+    }
+}
+
+export class InvoiceRepository extends Repository {
+    protected readonly model = new Model(
+        {
+            dni: { type: DatabaseType.INTEGER, options: { primaryKey: true }},
+            fecha_de_emision: { type: DatabaseType.DATE, options: { primaryKey: true }},
+            precio: { type: DatabaseType.FLOAT, options: { } },
+            fecha_de_pago: { type: DatabaseType.DATE, options: { allowNull: true }}
+        },
+        "Pyac",
+        "Facturas"
+    );
+}
+
+// Columna no lleva nombre xq el nombre lo define la key del atributo
+interface Column {
+    type: DatabaseType;
+    options: ColumnOptions;
+}
+
+// Los campos que son opcionales se van a rellenar con False en caso de ser undefined
+interface ColumnOptions {
+    primaryKey?: boolean;
+    allowNull?: boolean;
+}
+
+export const enum DatabaseType {
+    STRING = "STRING",
+    INTEGER = "INTEGER",
+    BOOLEAN = "BOOLEAN",
+    FLOAT = "FLOAT",
+    DATE = "DATE",
+    MODALIDADES = "MODALIDADES",
+    NIVELES = "NIVELES"
+}
+
+export class Model {
+    public readonly schema: string;
+    public readonly tableName: string;
+    private columns: Record<string, Column>;
+
+    public get allColumns(): string[] {
+        return Object.keys(this.columns);
+    }
+
+    public get primaryKeys(): string[] {
+        return this.allColumns.filter(key => this.columns[key]?.options.primaryKey);
+    }
+
+    public get nonPrimaryKeys(): string[] {
+        return this.allColumns.filter(key => !this.columns[key]?.options.primaryKey);
+    }
+
+    constructor(columns: Record<string, Column>, modelSchema: string, modelTable: string) {
+        this.schema = modelSchema;
+        this.tableName = modelTable;
+
+        // Convertimos los undefined en false
+        this.columns = {};
+        for (const key in columns) {
+            const col = columns[key]!;
+            this.columns[key] = {
+                type: col.type,
+                options: {
+                    primaryKey: col.options.primaryKey ?? false,
+                    allowNull: col.options.allowNull ?? false
+                }
+            };
+        }
+    }
+}
+
+/*
 export abstract class Repository<T extends PK, PK extends Record<string, any>> {
     public abstract readonly primaryKeys: (keyof PK)[];
     public abstract readonly nonPrimaryKeys: (keyof Omit<T, keyof PK>)[];
@@ -164,4 +340,4 @@ export class InvoiceRepository extends Repository<Invoice, InvoicePK> {
     public readonly nonPrimaryKeys: (keyof Omit<Invoice, keyof InvoicePK>)[] = ["precio", "fecha_de_pago"];
     public readonly schema: string = "pyac";
     public readonly table: string = "facturas";
-}
+}*/
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,11 +1,10 @@
 import { Router } from "express";
-import { Pool } from "pg";
 import { createAPICrud } from "../basicCrud.js"
-import { StudentRepository, AttendanceRepository, LevelRepository, InvoiceRepository, type Invoice } from "../database/repository.js";
 import { requireAuth } from "../middleware/auth.js"
 import authApiRoutes from "./authApi.js";
 import authPagesRoutes from "./authPages.js";
 import { poolDb } from "../database/client.js";
+import { AttendanceRepository, DatabaseType, InvoiceRepository, LevelRepository, Model, StudentRepository } from "../database/repository.js";
 
 const router = Router();
 
@@ -58,43 +57,54 @@ const studentColumnMeta: Record<string, ColumnMeta> = {
     },
 };
 
-// Tecnicamente createAPICrud podria ir adentro de Repository.
-// Hay que decidir si todos los Repositories van a tener un CRUD.
-// Por ahora quedan separados.
-const studentRepo = new StudentRepository(pool);
+const StudentModel = new Model(
+    {
+        dni: {
+            type: DatabaseType.INTEGER,
+            options: { primaryKey: true, allowNull: false }
+        },
+        nombre: {
+            type: DatabaseType.STRING,
+            options: { allowNull: false }
+        },
+        apellido: {
+            type: DatabaseType.STRING,
+            options: { allowNull: false }
+        },
+        curso: {
+            type: DatabaseType.STRING,
+            options: { allowNull: false }
+        },
+        modalidad: {
+            type: DatabaseType.MODALIDADES,
+            options: { allowNull: false }
+        },
+        nivel: {
+            type: DatabaseType.NIVELES,
+            options: { allowNull: false }
+        },
+        responsable_de_pagos: {
+            type: DatabaseType.STRING,
+            options: { allowNull: true }
+        },
+        responsable1: {
+            type: DatabaseType.STRING,
+            options: { allowNull: true }
+        }
+    },
+    "Pyac",
+    "Alumnos"
+);
+
+
+const studentRepo = new StudentRepository(poolDb);
 createAPICrud(router, studentRepo);
 
-const attendanceRepo = new AttendanceRepository(pool);
-//createAPICrud(router, attendanceRepo);
+const attendanceRepo = new AttendanceRepository(poolDb);
 
-const levelRepo = new LevelRepository(pool);
-createAPICrud(router, levelRepo);
+const levelRepo = new LevelRepository(poolDb);
 
-const invoiceRepo = new InvoiceRepository(pool);
-createAPICrud(router, invoiceRepo);
-
-// Create de presente
-router.post("/api/presentes", async (req, res) => {
-    try {
-        await attendanceRepo.create(req.body);
-        const student = await studentRepo.getByDNI(req.body.dni);
-        if (student.modalidad == "Eventual") {
-            const price = await levelRepo.getPrice(student.nivel);
-            const invoiceData: Invoice = {
-                "dni" : req.body.dni,
-                "fecha_de_emision" : new Date(),
-                "precio" : price,
-                "fecha_de_pago" : null
-            }
-            await invoiceRepo.create(invoiceData);
-        }
-        res.status(200).json({ ok: true });
-    }
-    catch (err) {
-        console.error(err);
-        res.status(500).json({ ok: false, error: (err as Error).message });
-    }
-});
+const invoiceRepo = new InvoiceRepository(poolDb);
 
 // Esto tambien lo podriamos generalizar para todos los Repositories.
 router.get("/app/alumnos", requireAuth, async (req, res) => {
